7e054695b3e40e45af9a944a7f13745d8d4c4b34
Arsalan831 <arsalanbashir831@gmail.com>
2024-11-07 03:16:10 +0500

    add lims and wqt application

--- server/employee_management/admin.py
+++ server/employee_management/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import CustomUser, Branches, Departments, Designations
+from .models import CustomUser, Branches, Departments, Designations , Attendance , DailyWorkReports, ApplicationLeave
 # Register your models here.
 
 admin.site.register(CustomUser)
 admin.site.register(Branches)
 admin.site.register(Departments)
 admin.site.register(Designations)
+admin.site.register(Attendance)
+admin.site.register(DailyWorkReports)  
+admin.site.register(ApplicationLeave)
+
---
+++ server/wqt/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WqtConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'wqt'
